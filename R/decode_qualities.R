6e28b5d371fe0c84a9e30e14e8ebdcc0d8a22994
laurabiggins <laurabiggins10@gmail.com>
2021-01-13 17:11:13 +0000

    added documentation for functions

--- R/decode_qualities.R
+++ R/decode_qualities.R
@@ -1,3 +1,15 @@
+#' Decode qualities
+#'
+#' Decode quality scores from fastq file.
+#'
+#' @param qualities set of quality scores from a fastq file
+#' @param offset phred type (numerical) must be 33 or 64
+#'
+#' @return numeric vector of Phred scores
+#' @export
+#'
+#' @examples
+#' decode_qualities("???#;ABAAAH")
 decode_qualities <- function(qualities, offset = 33){
 
   assertthat::assert_that(assertthat::is.scalar(offset), msg = "offset value must be a scalar")
--- R/read_fastq.R
+++ R/read_fastq.R
@@ -1,28 +1,47 @@
+#' Calculate GC content of DNA sequence
+#'
+#' @param seq DNA sequence (character string)
+#'
+#' @return single value of the proportion of G and C bases in the DNA sequence
+#' @export
+#'
+#' @examples
+#'gc_content("ACGTTTTGACGCATACCGAGCGCC")
 gc_content <- function(seq){
 
-  assertthat::assert_that(is.character(seq), msg = "sequence must be a character vector")
-  seq <- toupper(seq)
-  if(any(stringr::str_detect(seq, "[^GATC]"))) {
+  assertthat::assert_that(base::is.character(seq), msg = "sequence must be a character vector")
+  seq <- base::toupper(seq)
+  if(base::any(stringr::str_detect(seq, "[^GATC]"))) {
     warning("Non GATC characters found in sequence")
   }
   short_seq <- stringr::str_replace_all(seq,"[^GC]","")
-  nchar(short_seq)/nchar(seq)
+  base::nchar(short_seq)/base::nchar(seq)
 }
 
+#' Read fastq file
+#'
+#' @param filename fastq file to read in
+#'
+#' @return tibble with columns containing the id, sequence, id2 and quality score.
+#' Each read is on a new row.
+#' @export
+#'
+#' @examples
+#' fq_file <- read_fastq(system.file("good.fq", package = "fastqR"))
 read_fastq <- function(filename){
 
   assertthat::assert_that(assertthat::see_if(file.exists(filename)))
   assertthat::assert_that(assertthat::has_extension(filename, "fq"))
-  file_contents <- scan(filename,character())
+  file_contents <- base::scan(filename,character())
 
   id <- file_contents[c(TRUE, FALSE, FALSE, FALSE)]
-  assertthat::assert_that(!any(duplicated(id)), msg = "duplicated ids found")
+  assertthat::assert_that(!base::any(duplicated(id)), msg = "duplicated ids found")
 
   seq <- file_contents[c(FALSE, TRUE, FALSE, FALSE)]
   id2 <- file_contents[c(FALSE, FALSE, TRUE, FALSE)]
   qual <- file_contents[c(FALSE, FALSE, FALSE, TRUE)]
 
-  assertthat::assert_that(!any(nchar(seq) != nchar(qual)),
+  assertthat::assert_that(!base::any(base::nchar(seq) != nchar(qual)),
                           msg = "found unequal length of sequence and qualities")
 
   tibble::tibble(id, seq, id2, qual)
