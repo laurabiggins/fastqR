0018b7931d1e8ed1d716a612f237474e6bcce0ac
laurabiggins <laurabiggins10@gmail.com>
2021-01-13 17:11:51 +0000

    added vignette

---
+++ R/NULL.R
@@ -0,0 +1,15 @@
+#' fastqR: A package for importing and interpreting fastq files
+#'
+#' The fastqR package contains functions to import fastq files, to decode quality
+#' scores and to calculate GC content of reads.
+#'
+#' @section fastqR functions:
+#' read_fastq
+#'
+#' gc_content
+#'
+#' decode_qualities
+#'
+#' @docType package
+#' @name fastqR
+NULL
